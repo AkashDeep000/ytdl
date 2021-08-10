96c0e25e14268d4d52e7eaebc7fcd7c890b0546c
AkashDeep000 <akashdeepdas970@gmail.com>
2021-08-10 23:09:55 +0530

    1st

---
+++ index.js
@@ -0,0 +1,30 @@
+//const fs = require('fs');
+const ytdl = require('ytdl-core');
+const express = require('express');
+const app = express();
+
+// TypeScript: import ytdl from 'ytdl-core'; with --esModuleInterop
+// TypeScript: import * as ytdl from 'ytdl-core'; with --allowSyntheticDefaultImports
+// TypeScript: import ytdl = require('ytdl-core'); with neither of the above
+
+
+  
+  app.get('/', async function (req, res) {
+    
+    let id = req.query.id;
+    if (id) {
+      const head = {
+            
+            'Content-Type': 'video/mp4',
+        };
+        res.writeHead(200, head);
+ ytdl('https://www.youtube.com/watch?v=' + id).pipe(res);
+    
+    }else{
+      res.send('No Video ID')
+    }
+  
+    
+});
+  
+  app.listen(3000)
